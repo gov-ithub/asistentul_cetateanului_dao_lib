5e6805e66c146703b430f8664799747173e2bfdb
Cosmin <poterascosmin@yahoo.com>
2016-12-06 15:36:35 +0200

    Added logs to JsonMetadataConverter Removed source/target 1.5 from pom.xml

--- src/main/java/org/govithub/govac/dao/model/json/JsonMetadataConverter.java
+++ src/main/java/org/govithub/govac/dao/model/json/JsonMetadataConverter.java
@@ -4,18 +4,22 @@ import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import javax.validation.constraints.NotNull;
 
+import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 
 @Converter
 public class JsonMetadataConverter implements AttributeConverter<JsonMetadata, String> {
 
 	    private final static ObjectMapper objectMapper = new ObjectMapper();
+	    
+	    private Logger logger = Logger.getLogger(JsonMetadataConverter.class);
 
 	    @NotNull
 	    public String convertToDatabaseColumn(@NotNull JsonMetadata myCustomObject) {
 	        try {
 	            return objectMapper.writeValueAsString(myCustomObject);
 	        } catch (Exception ex) {
+	            logger.error("Error while converting to jsonb", ex);
 	            return null;
 	        }
 	    }
@@ -25,10 +29,8 @@ public class JsonMetadataConverter implements AttributeConverter<JsonMetadata, S
 	        try {
 	            return objectMapper.readValue(databaseDataAsJSONString, JsonMetadata.class);
 	        } catch (Exception ex) {
+	        	logger.error("Error while parsing jsonb string", ex);
 	            return null;
 	        }
 	    }
-
-	    
-    
 }
